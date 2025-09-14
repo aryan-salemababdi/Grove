ee2c4a028d4242dbf5c5bc8af157e3cfec34d23a
Aryan Salemabadi <aryanavatar1380@gmail.com>
2025-09-14 10:28:13 +0330

    fix: fix app module bug

--- cmd/new.go
+++ cmd/new.go
@@ -135,6 +135,10 @@ func (m *AppModule) Register(container *dig.Container, app *fiber.App) error {
 	if err := container.Provide(NewController); err != nil { return err }
 	return container.Invoke(func(ctrl *Controller) { ctrl.RegisterRoutes(app) })
 }
+
+func (m *AppModule) Middlewares() []string {
+	return []string{}
+}
 `
 	if err := writeFile(filepath.Join(moduleDir, "app.module.go"), module); err != nil {
 		return err
